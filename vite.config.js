5b3662b8b8874ffcb0759886351f2aa9fd7499db
Agustin Velazquez <agubolso2@gmail.com>
2025-04-16 13:00:58 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    // Esto asegura que las variables de entorno estén disponibles
+    "import.meta.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL),
+  },
+});
